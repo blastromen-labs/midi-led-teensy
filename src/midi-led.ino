7a47e1c12907243d963277bfb0d761232a551327
Sami Koskivaara <sami.koskivaara@gmail.com>
2024-08-24 13:25:33 +0300

    small cleanup

--- src/midi-led.ino
+++ src/midi-led.ino
@@ -1,21 +1,21 @@
 #include <OctoWS2811.h>
 
-const int NUM_PANELS = 4; // Change this to the number of panels you're using
-const int LEDS_PER_PANEL = 256;
-const int GROUPS_PER_PANEL = 4;
+const int NUM_PANELS = 1;       // 1-8
+const int LEDS_PER_PANEL = 512; // 256 or 512, if you chain two 256 panels together then this is 512
+const int GROUPS_PER_PANEL = 8; // 4 or 8, if you chain two 256 panels together then this is 8
 const int MIDI_CHANNEL = 1;
 
-const int ledsPerStrip = LEDS_PER_PANEL;
 const int totalLeds = NUM_PANELS * LEDS_PER_PANEL;
 const int ledsPerGroup = LEDS_PER_PANEL / GROUPS_PER_PANEL;
 const int numGroups = NUM_PANELS * GROUPS_PER_PANEL;
+const int totalNotes = numGroups * 3; // Total notes for all colors (blue, red, green)
 
-DMAMEM int displayMemory[ledsPerStrip * 6];
-int drawingMemory[ledsPerStrip * 6];
+DMAMEM int displayMemory[LEDS_PER_PANEL * 6];
+int drawingMemory[LEDS_PER_PANEL * 6];
 
 const int config = WS2811_RGB | WS2811_800kHz;
 
-OctoWS2811 leds(ledsPerStrip, displayMemory, drawingMemory, config);
+OctoWS2811 leds(LEDS_PER_PANEL, displayMemory, drawingMemory, config);
 
 struct GroupState
 {
@@ -37,32 +37,26 @@ void updateGroupLeds(int group)
     uint8_t r = groupStates[group].red;
     uint8_t g = groupStates[group].green;
     uint8_t b = groupStates[group].blue;
-
     int panelIndex = group / GROUPS_PER_PANEL;
     int groupWithinPanel = group % GROUPS_PER_PANEL;
-
     int startLed = groupWithinPanel * ledsPerGroup;
     int endLed = startLed + ledsPerGroup - 1;
 
     for (int i = startLed; i <= endLed; i++)
     {
-        leds.setPixel(i + panelIndex * LEDS_PER_PANEL, r, g, b); // Note: RGB order for WS2811
+        leds.setPixel(i + panelIndex * LEDS_PER_PANEL, r, g, b);
     }
     ledStateChanged = true;
 }
 
 void handleNoteEvent(byte channel, byte pitch, byte velocity, bool isNoteOn)
 {
-    int totalNotes = numGroups * 3; // Total notes for all colors
-    int notesPerColor = numGroups;  // Notes per color (blue, red, green)
-
     if (pitch < 128 - totalNotes || pitch > 127)
         return; // Ignore notes outside our range
 
     int noteIndex = 127 - pitch;
-    int colorIndex = noteIndex / notesPerColor;
-    int groupIndex = noteIndex % notesPerColor;
-
+    int colorIndex = noteIndex / numGroups;
+    int groupIndex = noteIndex % numGroups;
     uint8_t newVelocity = isNoteOn ? mapVelocityToBrightness(velocity) : 0;
 
     switch (colorIndex)
@@ -77,7 +71,6 @@ void handleNoteEvent(byte channel, byte pitch, byte velocity, bool isNoteOn)
         groupStates[groupIndex].green = newVelocity;
         break;
     }
-
     updateGroupLeds(groupIndex);
 }
 
