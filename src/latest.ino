df7f0dc443de38fa9c5997e94992bb063f5f26e6
“Satak” <sami.koskivaara@gmail.com>
2025-10-05 12:52:52 +0300

    add info

--- src/latest.ino
+++ src/latest.ino
@@ -25,6 +25,7 @@
 // 25.1.25, add white row strobe effects on MIDI channel 6 (notes 114-103)
 // 26.1.25, add midi CC for image scale on channel 4 (same CC 8 as video)
 // 26.1.25, add video mirror feature with MIDI CC 12 on channel 3 (value 127 = mirrored)
+// 5.11.25, add midi CC for video (CC 0) and image (CC 20) bank (value 0-127 = bank). folder structure /video/0/file.bin /image/0/file.bin
 // |c-1|c-2|c-3|c-4|c-5|
 // |---|---|---|---|---|
 // |1.1|3.2|4.1|6.2|7.1|
